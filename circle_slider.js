ca01b09c474495ab573d764e6dea7a786bb3a0ae
jja92 <57497216+jja92@users.noreply.github.com>
2020-02-14 13:46:16 -0800

    Update circle_slider.js

--- circle_slider.js
+++ circle_slider.js
@@ -1,43 +1,77 @@
-$(function () {
-    var $circle = $('#circle'),
-        $handler = $('#handler'),
-        $p = $('#test'),
-        handlerW2 = $handler.width() / 2,
-        rad = $circle.width() / 2,
-        offs = $circle.offset(),
-        elPos = {
-            x: offs.left,
-            y: offs.top
-        },
-        mHold = 0,
-        PI2 = Math.PI / 180;
-    $handler.mousedown(function () {
-        mHold = 1;
-    });
-    $(document).mousemove(function (e) {
-        if (mHold) {
-            var mPos = {
-                x: e.pageX - elPos.x,
-                y: e.pageY - elPos.y
-            },
-            atan = Math.atan2(mPos.x - rad, mPos.y - rad),
-                deg = -atan / PI2 + 180,
-                perc = (deg * 256 / 360) | 0,
-                X = Math.round(rad * Math.sin(deg * PI2)),
-                Y = Math.round(rad * -Math.cos(deg * PI2));
-            $handler.css({
-                left: X + rad - handlerW2,
-                top: Y + rad - handlerW2,
-                transform: 'rotate(' + deg + 'deg)'
-            });
-            $circle.css({
-                borderColor: "hsl(" + (perc * 360 / 256 - 100) + ",100%,50%)"
-            });
-            $p.html(perc).css({
-                color: "hsl(" + (perc * 360 / 256 - 100) + ",100%,50%)"
-            });
+$(function() {
+  var $container = $('#rotationSliderContainer');
+  var $slider = $('#rotationSlider');
+  var $degrees = $('#rotationSliderDegrees');
+
+  var sliderWidth = $slider.width();
+  var sliderHeight = $slider.height();
+  var radius = $container.width() / 2;
+  var deg = 0;
+
+  X = Math.round(radius * Math.sin(deg * Math.PI / 180));
+  Y = Math.round(radius * -Math.cos(deg * Math.PI / 180));
+  $slider.css({
+    left: X + radius - sliderWidth / 2,
+    top: Y + radius - sliderHeight / 2
+  });
+
+  var mdown = false;
+  $container
+    .mousedown(function(e) {
+      mdown = true;
+      e.originalEvent.preventDefault();
+    })
+    .mouseup(function(e) {
+      mdown = false;
+    })
+    .mousemove(function(e) {
+      if (mdown) {
+
+        // firefox compatibility
+        if (typeof e.offsetX === "undefined" || typeof e.offsetY === "undefined") {
+          var targetOffset = $(e.target).offset();
+          e.offsetX = e.pageX - targetOffset.left;
+          e.offsetY = e.pageY - targetOffset.top;
         }
-    }).mouseup(function () {
-        mHold = 0;
+
+        if ($(e.target).is('#rotationSliderContainer'))
+          var mPos = {
+            x: e.offsetX,
+            y: e.offsetY
+          };
+        else
+          var mPos = {
+            x: e.target.offsetLeft + e.offsetX,
+            y: e.target.offsetTop + e.offsetY
+          };
+
+        var atan = Math.atan2(mPos.x - radius, mPos.y - radius);
+        deg = -atan / (Math.PI / 180) + 180; // final (0-360 positive) degrees from mouse position 
+
+
+        // for attraction to multiple of 90 degrees
+        var distance = Math.abs(deg - (Math.round(deg / 90) * 90));
+
+        if (distance <= 5)
+          deg = Math.round(deg / 90) * 90;
+
+        if (deg == 360)
+          deg = 0;
+
+        X = Math.round(radius * Math.sin(deg * Math.PI / 180));
+        Y = Math.round(radius * -Math.cos(deg * Math.PI / 180));
+
+        $slider.css({
+          left: X + radius - sliderWidth / 2,
+          top: Y + radius - sliderHeight / 2
+        });
+
+        var roundDeg = Math.round(deg);
+
+        $degrees.html(roundDeg + '&deg;');
+        $('#imageRotateDegrees').val(roundDeg);
+
+      }
     });
+
 });
