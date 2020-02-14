dcccf781e2d4d0e99413cc26858b78389b8876c8
jja92 <57497216+jja92@users.noreply.github.com>
2020-02-14 13:30:51 -0800

    Update circle_slider.js

--- circle_slider.js
+++ circle_slider.js
@@ -1,17 +1,43 @@
-function SliderCtrl($scope) {
-  $scope.min = 10;
-  $scope.max = 100;
-  $scope.shape = 'Circle';
-  $scope.value = 20;
-
-  $scope.onSlide = function onSlide(value) {
-    console.log('on slide  ' + value);
-  }
-
-  $scope.onSlideEnd = function onSlideEnd(value) {
-    console.log('on slide end  ' + value);
-  }
-
-}
-
-angular.module('test', ['angular.circular-slider']).controller('SliderCtrl', SliderCtrl);
+$(function () {
+    var $circle = $('#circle'),
+        $handler = $('#handler'),
+        $p = $('#test'),
+        handlerW2 = $handler.width() / 2,
+        rad = $circle.width() / 2,
+        offs = $circle.offset(),
+        elPos = {
+            x: offs.left,
+            y: offs.top
+        },
+        mHold = 0,
+        PI2 = Math.PI / 180;
+    $handler.mousedown(function () {
+        mHold = 1;
+    });
+    $(document).mousemove(function (e) {
+        if (mHold) {
+            var mPos = {
+                x: e.pageX - elPos.x,
+                y: e.pageY - elPos.y
+            },
+            atan = Math.atan2(mPos.x - rad, mPos.y - rad),
+                deg = -atan / PI2 + 180,
+                perc = (deg * 256 / 360) | 0,
+                X = Math.round(rad * Math.sin(deg * PI2)),
+                Y = Math.round(rad * -Math.cos(deg * PI2));
+            $handler.css({
+                left: X + rad - handlerW2,
+                top: Y + rad - handlerW2,
+                transform: 'rotate(' + deg + 'deg)'
+            });
+            $circle.css({
+                borderColor: "hsl(" + (perc * 360 / 256 - 100) + ",100%,50%)"
+            });
+            $p.html(perc).css({
+                color: "hsl(" + (perc * 360 / 256 - 100) + ",100%,50%)"
+            });
+        }
+    }).mouseup(function () {
+        mHold = 0;
+    });
+});
